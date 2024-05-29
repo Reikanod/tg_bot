c46f7dd21758803b81e854493b5b8cea0379f9a0
Rey <reikanod@yandex.ru>
2024-05-29 07:37:48 +0300

    bvbvbvbvbvb

--- Bot.py
+++ Bot.py
@@ -1,25 +1,8 @@
-import telebot
-import lxml.html
-from lxml import etree
+from bs4 import BeautifulSoup
 import requests
 
-tree = etree.parse('Lenta.ru - Новости России и мира сегодня.html', lxml.html.HTMLParser())
-
-main_block = tree.findall('.//*[@id="body"]/div[3]/div[3]/main/div[2]/section[1]/div[1]/div[1]')
-for div in main_block:
-    for a in div:
-        print(a.text.encode('utf-8'))
-
-
-'''
-with open(r'D:\Work\Python\MyData\ReikanodBot_token.txt', 'r') as token:
-    bot = telebot.TeleBot(token.read())
-
-
-@bot.message_handler(content_types=['photo'])
-def start(message):
-    bot.reply_to(message, "Nice meme XDD")
-
-
-bot.polling(none_stop=True, interval=0)
-'''
+base = 'https://lenta.ru/'
+html = requests.get(base).content
+soup = BeautifulSoup(html, 'lxml')
+div = soup.find('div', id='question-mini-list')
+print(div)
