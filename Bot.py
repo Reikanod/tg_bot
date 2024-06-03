4a7080231320f820792121816e32274f5a94eec5
Reikanod <reikanod@yandex.ru>
2024-06-03 13:07:38 +0300

    qq

--- Bot.py
+++ Bot.py
@@ -34,10 +34,11 @@ class Converter:
             if text[0] in currencies.keys() and text[1] in currencies.keys():
                 cur_from = currencies[text[0]]
                 cur_to = currencies[text[1]]
-                amount_from = text[2]
+                amount_from = float(text[2])
                 result = requests.get(fr'https://min-api.cryptocompare.com/data/price?fsym={cur_from}&tsyms={cur_to}')
                 result = result.json() # словарь с парой "USD":100
-                amount_to = float(result[f'{cur_to}'] * amount_from) # запоминаем сумму в итоговой валюте
+                result = result[f'{cur_to}']
+                amount_to = result * amount_from # запоминаем сумму в итоговой валюте
                 amount_to = format(amount_to, '.2g') # итоговую сумму округляем до двух значимых цифр
                 result = f'{amount_from} {cur_from}({text[0]}) = {amount_to} {cur_to}({text[1]})'
                 bot.send_message(message.chat.id, result)
--- test.py
+++ test.py
@@ -11,17 +11,34 @@ currencies = {
     "вона" : "KRW"
 }
 
-message = 'рубль доллар 100'
+def round_to_signif(num):
+    num_str = str(num)
+    a = num_str.find('.')
+    num_str = num_str[a + 1:]
+    for i in range(len(num_str)):
+        if int(num_str[i]):
+            return round(num, i + 2)
+
+
+
+message = 'доллар рубль 100'
 
 text = message.lower().split()
 if text[0] in currencies.keys() and text[1] in currencies.keys():
     cur_from = currencies[text[0]]
     cur_to = currencies[text[1]]
-    amount_from = text[2]
+    amount_from = float(text[2])
     result = requests.get(fr'https://min-api.cryptocompare.com/data/price?fsym={cur_from}&tsyms={cur_to}')
     result = result.json()
-    print(type(result))
-    print(result)
+    result = result[f'{cur_to}']
+    amount_to = result * amount_from  # запоминаем сумму в итоговой валюте
+    print(type(amount_to), amount_to)
+    amount_to = round_to_signif(amount_to)
+    print(type(amount_to), amount_to)
+    result = f'{amount_from} {cur_from}({text[0]}) = {amount_to} {cur_to}({text[1]})'
+
+
+
 
     # amount_to = float(result[f'{cur_to}'] * amount_from)  # запоминаем сумму в итоговой валюте
     # amount_to = format(amount_to, '.2g')  # итоговую сумму округляем до двух значимых цифр
